05fe11ff79bbacf5751eacc08c852f7a2b54338c
Shawna Mays <shawnadmays@gmail.com>
2023-04-16 19:18:24 -0400

    birthchartcalc component added

---
+++ src/BirthChartCalculator.js
@@ -0,0 +1,24 @@
+import React from 'react';
+/* the import React line imports the React library, the core library for building user interfaces */
+
+/*the following defines a functional component that's called BirthChartCalculator.  
+A functional component is a way to define
+a React component using a function.  THis component represents the birth chart caluclator and returns JSX (Javascript XML) which is a syntax extension
+for JavaScript that allows you to write HTML like code in your JavaScript files */
+
+const BirthChartCalculator = () => {
+
+    return (
+        <div>
+            {/* TODO: Add components for birth chart calculator */}
+      <h1>Birth Chart Calculator</h1>
+      {/* TODO: Add form and inputs for user input */}
+      {/* TODO: Add logic to calculate birth chart */}
+      {/* TODO: Display birth chart results */}
+        </div>
+    );
+
+
+
+};
+export default BirthChartCalculator;
